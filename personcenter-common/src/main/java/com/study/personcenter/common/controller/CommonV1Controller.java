ea0e18cdac01f8ffad935d44323c65488a57fe1d
FlyHorizon <mzjtt004@126.com>
2026-06-16 15:51:36 +0800

    update

---
+++ personcenter-common/src/main/java/com/study/personcenter/common/controller/CommonV1Controller.java
@@ -0,0 +1,28 @@
+package com.study.personcenter.common.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 公共模块提供的 Controller，参照 UserV1Controller 的接口风格。
+ * 不依赖数据库，直接吐字符串返回值，方便做覆盖率测试。
+ */
+@RestController
+@RequestMapping("/api/common/v1")
+public class CommonV1Controller {
+
+    @RequestMapping(path = "/hello", method = RequestMethod.GET)
+    public String getHello() {
+        System.out.println("common one");
+        return "v1 hello";
+    }
+
+    @RequestMapping(path = "/helloTwo", method = RequestMethod.GET)
+    public String getTwo() {
+        System.out.println("common two");
+        return "v2 hello two";
+    }
+
+
+}
